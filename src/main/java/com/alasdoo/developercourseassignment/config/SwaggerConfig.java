4cfb6d209bf9d0c43af66f9eddb9460425b0eeef
katarinaStojkovic <katarina.stojkovic.3060@metropolitan.ac.rs>
2020-12-31 19:38:42 +0100

    Added BeanConfig, SwaggerConfig, TestConfig and error-message.properties

---
+++ src/main/java/com/alasdoo/developercourseassignment/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.alasdoo.developercourseassignment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile("dev")
+public class SwaggerConfig {
+    @org.springframework.context.annotation.Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
